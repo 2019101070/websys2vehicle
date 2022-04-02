3aacc00e75d50879f9662eb7a1feb82fe91536e5
Tim <forgamingaccsonly@gmail.com>
2022-04-02 11:53:34 +0800

    git commit -m make Vehicle

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+class Vehicle {
+    var make;
+
+    function set_make($make){
+    
+        $this->make = $make;
+    }
+    function get_make($make){
+        return $this->make;
+    }
+
+}
+
+$mustang = new Vehicle ();
+$toyota = new Vehicle();
+$mustang->set_make('MUSTANG');
+$toyota->set_make('TOYOTA');
+
+echo $mustang->get_make();
+echo "<br>";
+echo $toyota->get_make();
+?>
